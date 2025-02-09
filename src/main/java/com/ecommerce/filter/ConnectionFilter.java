5bf24291367f5cfd521050cd48bd4009919c48cd
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-08 20:14:18 -0500

    bugfix: try catch conn getConnection

--- src/main/java/com/ecommerce/filter/ConnectionFilter.java
+++ src/main/java/com/ecommerce/filter/ConnectionFilter.java
@@ -1,8 +1,14 @@
 package com.ecommerce.filter;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,24 +17,28 @@ import com.ecommerce.util.ConnectionDB;
 @WebFilter(urlPatterns = {"/api/*"})
 public class ConnectionFilter implements Filter {
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     // Add CORS headers
     HttpServletResponse res = (HttpServletResponse) response;
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Max-Age", "3600");
-    Connection conn = ConnectionDB.getConnection();
-    if ( conn.getAutoCommit() ) {
-      conn.setAutoCommit(false);
-    }
-    try {
-      request.setAttribute("connection", conn);
-      chain.doFilter(request, response);
-      conn.commit();
-    } catch (Exception e) {
-      conn.rollback();
-      ((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+    try (Connection conn = ConnectionDB.getConnection()) {
+      if ( conn.getAutoCommit() ) {
+        conn.setAutoCommit(false);
+      }
+      try {
+        request.setAttribute("connection", conn);
+        chain.doFilter(request, response);
+        conn.commit();
+      } catch (Exception e) {
+        conn.rollback();
+        ((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        e.printStackTrace();
+      }
+    } catch (SQLException e) {
+   
       e.printStackTrace();
     }
     
